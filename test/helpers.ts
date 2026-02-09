55734e126d8a7c4fceac6fbfa55069ed030646ed
Reid Moffat <18rem8@queensu.ca>
2026-02-09 17:36:34 -0500

    JSON validate + proper test file for base

--- test/helpers.ts
+++ test/helpers.ts
@@ -1,4 +1,5 @@
 import { execSync } from 'child_process';
+import { expect } from 'vitest';
 
 function runCLI(args: string): string {
     return execSync(`npm-popular-versions ${args}`, {
@@ -7,4 +8,37 @@ function runCLI(args: string): string {
     }).trim();
 }
 
-export { runCLI };
+/**
+ * Validates a JSON response
+ */
+function validateJson(json: string, packageName: string) {
+    expect(() => JSON.parse(json)).not.toThrow();
+    const data: { package: string; topVersions: object } = JSON.parse(json);
+
+    expect(data).toHaveProperty('package', packageName);
+    expect(data).toHaveProperty('topVersions');
+
+    // Top versions must have 10 key:value pairs in the format semver:number (sorted)
+    const topVersions: object = data.topVersions;
+
+    expect(typeof topVersions).toBe('object');
+    expect(topVersions).not.toBeNull();
+
+    const entries: [string, number][] = Object.entries(topVersions);
+    expect(entries).toHaveLength(10);
+
+    const values: number[] = [];
+    entries.forEach(([key, value]) => {
+        expect(key).toMatch(/^\d+\.\d+\.\d+(-[0-9A-Za-z.-]+)?$/);
+
+        expect(Number.isInteger(value)).toBe(true);
+        expect(value).toBeGreaterThan(0);
+
+        values.push(value);
+    });
+
+    const sortedValues: number[] = [...values].sort((a, b) => b - a);
+    expect(values).toEqual(sortedValues);
+}
+
+export { runCLI, validateJson };
--- test/json.test.ts
+++ test/json.test.ts
@@ -1,31 +1,19 @@
-import { suite, test, expect } from 'vitest';
-import { runCLI } from './helpers.ts';
+import { suite, test } from 'vitest';
+import { runCLI, validateJson } from './helpers.ts';
 
 suite('JSON checks (no options)', () => {
     test('firebase  --json', () => {
         const output = runCLI('firebase --json');
-        expect(output).toContain('firebase');
+        validateJson(output, 'firebase');
     });
 
     test('@types/node  --json', () => {
         const output = runCLI('@types/node  --json');
-        expect(output).toContain('@types/node');
+        validateJson(output, '@types/node');
     });
 
     test('@prisma/client  --json', () => {
         const output = runCLI('@prisma/client  --json');
-        expect(output).toContain('@prisma/client');
-    });
-
-    test('should handle invalid package names gracefully', () => {
-        runCLI('invalidpkgsjahflasaksfhjsafjldsjldsflkdsa');
-    });
-
-    test('should respect --json flag', () => {
-        const output = runCLI('firebase --json');
-        expect(() => JSON.parse(output)).not.toThrow();
-        const data = JSON.parse(output);
-        expect(data).toHaveProperty('package', 'firebase');
-        expect(data).toHaveProperty('topVersions');
+        validateJson(output, '@prisma/client');
     });
 });
