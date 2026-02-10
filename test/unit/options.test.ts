f36d698d53cf3697221d5c109a8a9e05d1f3470f
Reid Moffat <18rem8@queensu.ca>
2026-02-09 20:51:19 -0500

    Limit tests

--- test/unit/options.test.ts
+++ test/unit/options.test.ts
@@ -1,9 +1,19 @@
-import { suite, test, expect } from 'vitest';
-import { runCLI } from '../helpers.ts';
+import { suite, test } from 'vitest';
+import { runCLI, validateJson, validateSimple, validateStandard } from '../helpers.ts';
 
 suite('Tests with options', () => {
-    test('Fetch JSON', () => {
-        const output = runCLI('firebase --json');
-        expect(output).toContain('firebase');
+    test('Limit 900 firebase standard', () => {
+        const output: string = runCLI('firebase --limit 900');
+        validateStandard(output, 'firebase', 900);
+    });
+
+    test('Limit 10000 @prisma/client json', () => {
+        const output: string = runCLI('@prisma/client -l 10000 --json');
+        validateJson(output, '@prisma/client', 10000);
+    });
+
+    test('Limit 2000 istanbul-lib-instrument simple', () => {
+        const output: string = runCLI('istanbul-lib-instrument -l 2000 --simple');
+        validateSimple(output, 'istanbul-lib-instrument', 2000);
     });
 });
